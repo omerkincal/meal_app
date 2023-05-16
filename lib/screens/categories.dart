be21c4c030e92332506ea2f332a610953081789e
ÖMER KINCAL <202804010@ogr.cbu.edu.tr>
2023-05-16 12:35:57 +0300

    first commit

---
+++ lib/screens/categories.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisExtent: 20,
+          childAspectRatio: 20,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text('1'),
+          Text('2'),
+          Text('3'),
+          Text('4'),
+          Text('5'),
+          Text('6'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
