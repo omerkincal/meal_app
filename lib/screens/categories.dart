ef3b7ebf4db183e917b984ec2e9c6a3fbf900b21
ÖMER KINCAL <202804010@ogr.cbu.edu.tr>
2023-05-26 15:30:08 +0300

    selecting favorites

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,9 +3,15 @@ import 'package:meal_app/data/dummy_data.dart';
 import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
 import '../models/category.dart';
+import '../models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+  });
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectedCategory(BuildContext context, Category category) {
     final filteredMeal = dummyMeals
@@ -15,8 +21,11 @@ class CategoriesScreen extends StatelessWidget {
     Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (ctx) =>
-                MealsScreen(title: category.title, meals: filteredMeal)));
+            builder: (ctx) => MealsScreen(
+                  title: category.title,
+                  meals: filteredMeal,
+                  onToggleFavorite: onToggleFavorite,
+                )));
   }
 
   @override
